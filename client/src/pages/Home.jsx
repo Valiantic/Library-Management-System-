8ddac2c0b892db762aa453782fc2a25a27551c1a
iskodemain <francislingan.onlineclass.94@gmail.com>
2025-12-27 13:47:26 +0800

    feat(ui): add sidebar toggle button to navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     <>
     <Navbar/>
     <Sidebar/>
-    <div className="min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8 pl-6">
+    <div className="min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8">
       <div className="max-w-7xl mx-auto">
         {/* Top Section with Cards */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-6">
